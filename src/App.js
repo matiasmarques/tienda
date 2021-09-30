57caa94e5795b1764550672609fd2c0239b0004a
matiasmarques <64795188+matiasmarques@users.noreply.github.com>
2021-09-30 14:44:43 -0300

    correcion estilos

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { ItemListContainer } from './components/container/ItemListContainer';
 function App() {
   return (
     <div>
-    <h1>Mi tienda en react</h1>
+    <h1 className='titulo'>Mi tienda en react</h1>
     <NavBar />
 	  <ItemListContainer greetings="Catálogo de productos"/>
     </div>
